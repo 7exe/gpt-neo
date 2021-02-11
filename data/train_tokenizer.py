77f97e77df7f5149ee316588229c14eae161e962
7exe <45569190+7exe@users.noreply.github.com>
2021-02-11 14:36:08 +0100

    Update train_tokenizer.py

--- data/train_tokenizer.py
+++ data/train_tokenizer.py
@@ -13,61 +13,60 @@ from tqdm import tqdm
 
 # parser
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--base_dir", type=str, help="Path to where your files are located. Files ending in .zst are treated as \
-                    archives, all others as raw text.")
-parser.add_argument("--output_dir", type=str, default="tokenizers", help="Where to put the tokenizer")
-parser.add_argument("--file_type", type=str, choices=["xz", "txt"], default="xz", help="Extension of file to parse")
-parser.add_argument("--vocab_size", type=int, help="Size of vocabulary", required = True)
-args = parser.parse_args()
 
 # main script
 
-data_path = Path(args.base_dir)
-archives = glob(str(data_path / f"*.{args.file_type}"))
+def tokenize_str(base_dir="", vocab_size=-1, output_dir="tokenizers")
+  
+  assert vocab_size == -1
 
-out_path = Path(args.output_dir)
 
-if os.path.exists(out_path):
-    shutil.rmtree(out_path)
+  data_path = Path(args.base_dir)
+  archives = glob(str(data_path / f"*.{args.file_type}"))
 
-if not out_path.is_dir():
-    out_path.mkdir()
+  out_path = Path(args.output_dir)
 
-    for arch in tqdm(archives):
-        name = os.path.basename(arch).split(".")[0] + ".txt"
-        fp = out_path / name
+  if os.path.exists(out_path):
+      shutil.rmtree(out_path)
 
-        if args.file_type == 'xz':
-            g = Reader(arch).stream_data()
+  if not out_path.is_dir():
+      out_path.mkdir()
 
-            with open(fp, "w") as f:
-                for s in g:
-                    f.write(s)
-                    f.write("\n\n")
-        elif args.file_type == 'txt':
-            shutil.copyfile(str(arch), str(fp))
+      for arch in tqdm(archives):
+          name = os.path.basename(arch).split(".")[0] + ".txt"
+          fp = out_path / name
 
-data_files = glob(str(out_path / "*.txt"))
-data_files = random.sample(data_files, int(0.2 * len(data_files)))
+          if args.file_type == 'xz':
+              g = Reader(arch).stream_data()
 
-assert len(data_files) > 0, 'No data files found'
+              with open(fp, "w") as f:
+                  for s in g:
+                      f.write(s)
+                      f.write("\n\n")
+          elif args.file_type == 'txt':
+              shutil.copyfile(str(arch), str(fp))
 
-# Initialize a tokenizer
-tokenizer = Tokenizer(models.BPE())
+  data_files = glob(str(out_path / "*.txt"))
+  data_files = random.sample(data_files, int(0.2 * len(data_files)))
 
-# Customize pre-tokenization and decoding
-tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
-tokenizer.decoder = decoders.ByteLevel()
-tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
-tokenizer.normalizer = NFKC()
+  assert len(data_files) > 0, 'No data files found'
 
-# And then train
-trainer = trainers.BpeTrainer(vocab_size=args.vocab_size, min_frequency=2, special_tokens=["<|endoftext|>", "<|padding|>"])
-tokenizer.train(trainer, data_files)
+  # Initialize a tokenizer
+  tokenizer = Tokenizer(models.BPE())
 
-# And Save it
-tokenizer_path = out_path / "byte-level-bpe.tokenizer.json"
-tokenizer.save(str(tokenizer_path), pretty=True)
+  # Customize pre-tokenization and decoding
+  tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=True)
+  tokenizer.decoder = decoders.ByteLevel()
+  tokenizer.post_processor = processors.ByteLevel(trim_offsets=True)
+  tokenizer.normalizer = NFKC()
 
-print(f'tokenizer saved at {str(tokenizer_path)}')
+  # And then train
+  trainer = trainers.BpeTrainer(vocab_size=args.vocab_size, min_frequency=2, special_tokens=["<|endoftext|>", "<|padding|>"])
+  tokenizer.train(trainer, data_files)
+
+  # And Save it
+  tokenizer_path = out_path / "byte-level-bpe.tokenizer.json"
+  tokenizer.save(str(tokenizer_path), pretty=True)
+
+  print(f'tokenizer saved at {str(tokenizer_path)}')
+  return tokenizer_path
